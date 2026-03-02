981eb88d56b68ed64acf76096a8027409bbc1ebb
Dani <truzohay@gmail.com>
2026-03-02 22:56:57 +0100

    Simulacion

--- Lab2_FIR_filter_implementation/src/TopLevelEntity.vhdl
+++ Lab2_FIR_filter_implementation/src/TopLevelEntity.vhdl
@@ -25,15 +25,21 @@ port(
 );
 end component;
 
-component FIR_pipeline is 
-    generic(
-        a0 : integer := -8;
-        a1 : integer := -14;
-        a2 : integer := -1;
-        a3 : integer := 20;
-        a4 : integer := 52;
-        a5 : integer := 73
-    );
+component FIR is 
+	 generic(
+		a0 : integer := -8;
+		a1 : integer := -14;
+		a2 : integer := -1;
+		a3 : integer := 20;
+		a4 : integer := 52;
+		a5 : integer := 73;
+		a6 : integer := 73;
+		a7 : integer := 52;
+		a8 : integer := 20;
+		a9 : integer := -1;
+		a10 : integer := -14;
+		a11 : integer := -8
+	);
 	port(
 		clk,rst,enable : in std_logic;
 		DataIn: in signed (7 downto 0);
@@ -78,7 +84,7 @@ end process;
 enable10k <= '1' when cnt_10k=9999 else '0';
 
 
-FIR1 : FIR_pipeline
+FIR1 : FIR
 port map(
  clk => clk,
  rst => rst,
